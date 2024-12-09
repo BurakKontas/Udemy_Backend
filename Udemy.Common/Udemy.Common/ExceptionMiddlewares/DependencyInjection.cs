a538371e1d645165e5a0ff712a7a15cb9df4fc89
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-12-09 19:25:26 +0300

    Refactor exception handling and update Dockerfiles
    Updated Dockerfiles for `Udemy.APIGateway` and `Udemy.Auth` projects to expose port 8081 and include additional dependencies. Removed `ArgumentNullExceptionHandler.cs`, `AuthenticationFailureExceptionHandler.cs`, and `ValidationExceptionHandler.cs` files. Updated `DependencyInjection.cs` to remove registration of these handlers, now using only `GlobalExceptionHandler`. Enhanced `GlobalExceptionHandler.cs` to handle `ArgumentNullException`, `AuthenticationFailureException`, and `ValidationException` exceptions with a new `HandleExceptionAsync` method.

--- Udemy.Common/Udemy.Common/ExceptionMiddlewares/DependencyInjection.cs
+++ Udemy.Common/Udemy.Common/ExceptionMiddlewares/DependencyInjection.cs
@@ -8,8 +8,6 @@ public static class DependencyInjection
     public static IServiceCollection AddExceptionMiddlewares(this IServiceCollection services)
     {
         // Register custom middleware services
-        services.AddTransient<ArgumentNullExceptionHandler>();
-        services.AddTransient<ValidationExceptionHandler>();
         services.AddTransient<GlobalExceptionHandler>();
 
         return services;
@@ -17,8 +15,6 @@ public static class DependencyInjection
 
     public static WebApplication AddExceptionMiddlewares(this WebApplication app)
     {
-        app.UseMiddleware<ArgumentNullExceptionHandler>();
-        app.UseMiddleware<ValidationExceptionHandler>();
         app.UseMiddleware<GlobalExceptionHandler>();
 
         return app;
--- Udemy.Common/Udemy.Common/ExceptionMiddlewares/GlobalExceptionHandler.cs
+++ Udemy.Common/Udemy.Common/ExceptionMiddlewares/GlobalExceptionHandler.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -12,21 +14,42 @@ public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IM
         {
             await next(context);
         }
+        catch (Exception exception) when (exception is ArgumentNullException)
+        {
+            await HandleExceptionAsync(context, exception, "ArgumentNullFailure", StatusCodes.Status400BadRequest);
+        }
+        catch (Exception exception) when (exception is AuthenticationFailureException)
+        {
+            await HandleExceptionAsync(context, exception, "AuthenticationFailure", StatusCodes.Status403Forbidden);
+        }
+        catch (Exception exception) when (exception is ValidationException)
+        {
+            await HandleExceptionAsync(context, exception, "ValidationFailure", StatusCodes.Status400BadRequest);
+        }
         catch (Exception exception)
         {
-            var exceptionId = Guid.NewGuid();
-            var exceptionMessage = exception.Message;
-            logger.LogError(exception, "Id: {@Id}\nError Message: {@ExceptionMessage}\nStackTrace: {@StackTrace}", exceptionId, exceptionMessage, exception.StackTrace);
-
-            var problemDetails = new ProblemDetails
-            {
-                Title = $"Server Error: {exceptionMessage}",
-                Status = StatusCodes.Status500InternalServerError,
-                Instance = context.Request.Path,
-                Detail = exceptionId.ToString(),
-            };
-            context.Response.StatusCode = problemDetails.Status.Value;
-            await context.Response.WriteAsJsonAsync(problemDetails);
+            await HandleExceptionAsync(context, exception, "ServerError", StatusCodes.Status500InternalServerError);
         }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception, string type, int statusCode)
+    {
+        var exceptionId = Guid.NewGuid();
+        var exceptionMessage = exception.Message;
+
+        logger.LogError(exception, "Id: {@Id}\nError Message: {@ExceptionMessage}\nStackTrace: {@StackTrace}",
+            exceptionId, exceptionMessage, exception.StackTrace);
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = $"{type} Error: {exceptionMessage}",
+            Type = type,
+            Status = statusCode,
+            Detail = exceptionId.ToString(),
+            Instance = context.Request.Path
+        };
+
+        context.Response.StatusCode = problemDetails.Status.Value;
+        await context.Response.WriteAsJsonAsync(problemDetails);
     }
 }
