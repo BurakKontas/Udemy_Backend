366a8feca09bc00b7fec504b66f28bea6118c224
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-12-30 18:25:06 +0300

    Add EndpointFilter model binder for query parameter binding
    Introduce a new namespace `Udemy.Common.Attributes` and add using directives for ASP.NET Core MVC components. Add `EndpointFilterModelBinder` class implementing `IModelBinder` to bind query parameters from HTTP requests to an `EndpointFilter` object. The `BindModelAsync` method extracts parameters like `start`, `limit`, `sortBy`, `sortOrder`, `filterBy`, and `filterValue` to create an `EndpointFilter` object, which is then set as the result of the model binding process. Introduce `EndpointFilter` record with properties for filtering logic and decorate it with the `ModelBinder` attribute specifying `EndpointFilterModelBinder` as its binder type.

---
+++ Udemy.Common/Udemy.Common/ModelBinder/EndpointFilterModelBinder.cs
@@ -0,0 +1,45 @@
+﻿namespace Udemy.Common.Attributes;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+public class EndpointFilterModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        var query = bindingContext.HttpContext.Request.Query;
+
+        var start = int.TryParse(query["start"], out var s) ? s : 0;
+        var limit = int.TryParse(query["limit"], out var l) ? l : 10;
+        var sortBy = query["sortBy"].FirstOrDefault() ?? "Id";
+        var sortOrder = query["sortOrder"].FirstOrDefault()?.ToLower() ?? "asc";
+        var filterBy = query["filterBy"].FirstOrDefault();
+        var filterValue = query["filterValue"].FirstOrDefault();
+
+        var filter = new EndpointFilter
+        {
+            Start = start,
+            Limit = limit,
+            SortBy = sortBy,
+            SortOrder = sortOrder,
+            FilterBy = filterBy,
+            FilterValue = filterValue
+        };
+
+        bindingContext.Result = ModelBindingResult.Success(filter);
+        return Task.CompletedTask;
+    }
+}
+
+// Record to encapsulate filtering logic
+[ModelBinder(BinderType = typeof(EndpointFilterModelBinder))]
+public record EndpointFilter
+{
+    public int Start { get; init; }
+    public int Limit { get; init; } = 10;
+    public string SortBy { get; init; } = "Id";
+    public string SortOrder { get; init; } = "asc";
+    public string? FilterBy { get; init; }
+    public string? FilterValue { get; init; }
+}
