51e52d1e4c3730cd542034f775fcccec4c0d88b5
Arda Burak Kontaş <aburakkontas@hotmail.com>
2025-01-04 11:33:44 +0300

    Add BlobController and Docker support for Udemy.CDN.API
    Added a new BlobController class with endpoints for blob operations in BlobController.cs. Introduced a Dockerfile for building and running the Udemy.CDN.API project using .NET 8.0. Updated Program.cs to configure services and middleware, including Swagger, authentication, API versioning, Consul, and exception handling. Added launchSettings.json for different profiles, and created Udemy.CDN.API.csproj targeting .NET 8.0. Added appsettings files for configuration. Created new project files for Udemy.CDN.Application, Udemy.CDN.Domain, and Udemy.CDN.Infrastructure. Updated Udemy.sln to include new projects. Modified docker-compose files to include the udemy.cdn.api service.

---
+++ Udemy.CDN/Udemy.CDN.API/Controllers/BlobController.cs
@@ -0,0 +1,65 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Udemy.Common.ModelBinder;
+
+namespace Udemy.CDN.API.Controllers;
+
+[Route("/v{version:apiVersion}/blob")]
+[ApiController]
+[ApiVersion("1.0")]
+public class BlobController : ControllerBase
+{
+    [HttpGet("{blobId:guid}")]
+    public async Task<IResult> GetBlob(Guid blobId)
+    {
+        //var result = await _blobService.GetByIdAsync(blobId);
+        //return TypedResults.Ok(result);
+
+        return TypedResults.Empty;
+    }
+
+    [HttpPost("upload")]
+    public async Task<IResult> UploadBlob([FromBody] UploadBlobRequest request)
+    {
+        //var result = await _blobService.UploadAsync(request);
+        //return TypedResults.Redirect($"get/{result}");
+
+        return TypedResults.Empty;
+    }
+
+    [HttpDelete("{blobId:guid}")]
+    public async Task<IResult> DeleteBlob(Guid blobId)
+    {
+        //await _blobService.DeleteAsync(blobId);
+        return TypedResults.NoContent();
+    }
+
+    [HttpGet("download/{blobId:guid}")]
+    public async Task<IResult> DownloadBlob(Guid blobId)
+    {
+        //var result = await _blobService.DownloadAsync(blobId);
+        //return TypedResults.Ok(result);
+
+        return TypedResults.Empty;
+    }
+
+    [HttpGet("list")]
+    public async Task<IResult> ListBlobs(EndpointFilter filter)
+    {
+        //var result = await _blobService.ListAsync();
+        //return TypedResults.Ok(result);
+
+        return TypedResults.Empty;
+    }
+
+    [HttpGet("search")]
+    public async Task<IResult> ListBlobs([FromQuery] string prefix, EndpointFilter filter)
+    {
+        //var result = await _blobService.ListAsync(container);
+        //return TypedResults.Ok(result);
+
+        return TypedResults.Empty;
+    }
+}
+
+public record UploadBlobRequest(string Name, string ContentType, byte[] Data);
